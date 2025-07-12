adb9cefaa87cda2b4a2b6f1459a7026605f81c05
javierdebug <jvargas071@gmail.com>
2025-07-12 16:44:45 -0500

    Update .fzf.zsh with $HOME for correct path

--- fzf/.fzf.zsh
+++ fzf/.fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/javierd/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/javierd/.fzf/bin"
+if [[ ! "$PATH" == *"$HOME/.fzf/bin"* ]]; then
+  PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/javierd/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/javierd/.fzf/shell/key-bindings.zsh"
+source "$HOME/.fzf/shell/key-bindings.bash"
